8f1c7b7aa0718d8fb8843c0d595bb7b3988f2a40
awolter27 <awolter@butler.edu>
2023-05-12 22:19:46 -0400

    added in resume

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,14 +1,23 @@
-import Typed from 'typed.js';
+import React, { useRef, useEffect } from 'react';
+import Typed from 'typed.js'
 
 function About({ about }) {
+    const textRef = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(textRef.current, {
+            strings: ["Hi, I'm Allie ... "],
+            typeSpeed: 45,
+            showCursor: false,
+        })
+    }, []);
+
     return (
-        <>
+        <div className='about-div'>
             <h1 className='about-heading'>ABOUT</h1>
-            <div className='about'>
-                <h2 className='name-about'>{about.intro}</h2>
-                <p className='bio'>{about.bio}</p>
-            </div>
-        </>
+            <h2 className='about-name' ref={textRef}></h2>
+            <p className='about-bio'>{about.bio}</p>
+        </div>
     );
 }
 
